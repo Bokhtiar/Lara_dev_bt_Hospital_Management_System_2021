f43b6daa4e6a4bdb05b3d76d545d26954bdb83bc
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2021-10-23 16:27:24 +0600

    admin breadcrumb section done

---
+++ resources/views/admin/about.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.admin.app')
+@section('admin_content')
+    <x-header data="about"/>
+@endsection
--- resources/views/layouts/admin/partial/sidebar.blade.php
+++ resources/views/layouts/admin/partial/sidebar.blade.php
@@ -1,10 +1,10 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="{{asset('/images/icon.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+    <a href="{{ route('admin.dashboard') }}" class="brand-link">
+      <img src="{{asset('/images/hms_icon.jpg')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">
-        Inventory(POS)
+        HMS
     </span>
     </a>
 
@@ -27,7 +27,7 @@
                with font-awesome or any other icon font library -->
 
           <li class="nav-item">
-            <a href="{{url('/')}}" class="nav-link">
+            <a href="{{ route('admin.dashboard') }}" class="nav-link">
                 <i class="nav-icon fa fa-dashboard"></i>
               <p>
                 Dashboard
@@ -45,13 +45,13 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="@route('purchase.index')" class="nav-link">
+                <a href="" class="nav-link">
                   <i class="fa fa-circle-o nav-icon"></i>
                   <p>Purchase</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="@route('purchase.date.filtering')" class="nav-link">
+                <a href="" class="nav-link">
                   <i class="fa fa-circle-o nav-icon"></i>
                   <p>Purchase Date Filtering</p>
                 </a>
